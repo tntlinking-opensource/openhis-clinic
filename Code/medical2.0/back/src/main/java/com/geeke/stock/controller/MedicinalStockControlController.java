b803ffd2c0fdf3e1cf7f4f84d73d5330fd2a3947
YingZ <478261895@qq.com>
2025-06-30 15:23:22 +0800

    feat:电子处方医保接口完善
    Former-commit-id: d0e01afff6a4ee96e7cb5a03774d4477f8891504

--- Code/medical2.0/back/src/main/java/com/geeke/stock/controller/MedicinalStockControlController.java
+++ Code/medical2.0/back/src/main/java/com/geeke/stock/controller/MedicinalStockControlController.java
@@ -55,6 +55,7 @@ public class MedicinalStockControlController extends BaseController {
         if("false".equals(searchParams.getYpType()) || "0".equals(searchParams.getYpType()) ) {
            result = medicinalStockControlService.listAllByCompany(searchParams.getParams(), searchParams.getOrderby());
         }else{
+
          result =    medicinalStockControlService.listPreAll(searchParams.getParams(), searchParams.getOrderby());
         }
         return ResponseEntity.ok(ResultUtil.successJson(result));
