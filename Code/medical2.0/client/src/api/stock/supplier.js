be172823746fc096018f60f915d483bdae00d1e3
Hzx <478261895@qq.com>
2026-06-15 10:12:46 +0800

    refactor: clean up supplier API file format

--- Code/medical2.0/client/src/api/stock/supplier.js
+++ Code/medical2.0/client/src/api/stock/supplier.js
@@ -1,58 +1,28 @@
+/**
+ * 供应商管理 API
+ */
 import request from '@/utils/request'
 
 export const getSupplierById = (id) =>
-    request({
-        url: '/stock/supplier/' + id,
-        method: 'get'
-    })
+  request({ url: '/stock/supplier/' + id, method: 'get' })
 
 export const listSupplierPage = (search) =>
-    request({
-        url: '/stock/supplier/list',
-        method: 'post',
-        data: search
-    })
+  request({ url: '/stock/supplier/list', method: 'post', data: search })
 
 export const listSupplierAll = (search) =>
-    request({
-        url: '/stock/supplier/listAll',
-        method: 'post',
-        data: search
-    })
+  request({ url: '/stock/supplier/listAll', method: 'post', data: search })
 
+export const saveSupplier = (supplier) =>
+  request({ url: '/stock/supplier/save', method: 'post', data: supplier })
 
-export const saveSupplier = (supplier) => 
-    request({
-        url: '/stock/supplier/save',
-        method: 'post',
-        data: supplier
-    })
-  
 export const deleteSupplier = (supplier) =>
-    request({
-        url: '/stock/supplier/delete',
-        method: 'post',
-        data: supplier
-    })
-    
+  request({ url: '/stock/supplier/delete', method: 'post', data: supplier })
+
 export const bulkInsertSupplier = (suppliers) =>
-    request({
-        url: '/stock/supplier/bulkInsert',
-        method: 'post',
-        data: suppliers
-    })
-    
+  request({ url: '/stock/supplier/bulkInsert', method: 'post', data: suppliers })
+
 export const bulkUpdateSupplier = (suppliers) =>
-    request({
-        url: '/stock/supplier/bulkUpdate',
-        method: 'post',
-        data: suppliers
-    })
+  request({ url: '/stock/supplier/bulkUpdate', method: 'post', data: suppliers })
 
 export const bulkDeleteSupplier = (suppliers) =>
-    request({
-        url: '/stock/supplier/bulkDelete',
-        method: 'post',
-        data: suppliers
-    })
-    
+  request({ url: '/stock/supplier/bulkDelete', method: 'post', data: suppliers })
